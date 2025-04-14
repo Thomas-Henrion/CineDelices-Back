53cbcc57f2f6f94a240d5d0da0deeab882775a32
M1000fr <formentel.remi@gmail.com>
2025-04-14 10:38:42 +0200

    feat: enhance user registration with comments and refresh token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -9,32 +9,50 @@ import { EmailParams, Recipient } from "mailersend";
 export default {
 	login: (req: Request, res: Response) => {},
 	register: async (req: Request, res: Response) => {
+		// Récupère les informations de l'utilisateur depuis la requête
 		const { email, password, name } = req.body as {
 			email: string;
 			password: string;
 			name: string;
 		};
 
+		// Vérifie si l'utilisateur existe déjà
 		const user = await User.findOne({ where: { email } });
 		if (user) {
 			res.status(400).json({ message: "User already exists" });
 			return;
 		}
 
+		// On hashe le mot de passe
 		const hashedPassword = await argon2.hash(password);
+
+		// Création d'un code de vérification aléatoire
 		const randomVerificationCode = Math.floor(
 			1000 + Math.random() * 9000,
 		).toString();
+
+		// Création de l'utilisateur dans la base de données
 		const newUser = await User.create({
 			name,
 			email,
 			password: hashedPassword,
 			vericationCode: randomVerificationCode,
 		});
+
+		// Création du token JWT et du refresh token
 		const token = jsonwebtoken.sign({ id: newUser.id }, dotenv.JWT.SECRET, {
-				expiresIn: "10m",
+			expiresIn: "10m",
 		});
 
+		const refreshToken = jsonwebtoken.sign(
+			{ id: newUser.id },
+			dotenv.JWT.REFRESH_SECRET,
+			{
+				expiresIn: "7d",
+			},
+		);
+
+		// Envoi de l'email de vérification
 		const recipients = [new Recipient(email, name)];
 
 		const emailParams = new EmailParams()
@@ -54,6 +72,7 @@ export default {
 			return;
 		});
 
+		// Retourne le token et le refresh token
 		res.status(201).json({
 			message: "User created successfully",
 			user: {
@@ -62,6 +81,7 @@ export default {
 				email: newUser.email,
 			},
 			token,
+			refreshToken,
 		});
 	},
 	confirmEmail: (req: Request, res: Response) => {},
