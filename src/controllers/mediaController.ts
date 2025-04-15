8948924608202262155ed3898acde250a095bfdc
Thomas-Gambin <t.gambin.pro@gmail.com>
2025-04-15 16:02:00 +0200

    feat : add media filter

--- src/controllers/mediaController.ts
+++ src/controllers/mediaController.ts
@@ -1,15 +1,51 @@
 import type { Request, Response } from "express";
 import { Media, Recipe } from "../database/association";
+import { Op } from "sequelize";
+import type { FindOptions } from "sequelize";
 
 export default {
 	getAllMedias: async (req: Request, res: Response): Promise<void> => {
-		try {
-			const media = await Media.findAll();
-			res.status(200).json(media);
-		} catch (error) {
-			res.status(500).json({ error: "Internal Server Error" });
+		// try {
+		// 	const media = await Media.findAll();
+		// 	res.status(200).json(media);
+		// } catch (error) {
+		// 	res.status(500).json({ error: "Internal Server Error" });
+		// }
+		const {
+			title, 
+			limit = "25", 
+			offset = "0", 
+		} = req.query as {
+			title: string;
+			limit: string;
+			offset: string;
+		};
+
+		// Conversion des paramètres de pagination en nombres
+		const numLimit = Number.parseInt(limit, 10);
+		const numOffset = Number.parseInt(offset, 10);
+
+		let where = {};
+		if (title) {
+			where = {
+				...where,
+				title: {
+					[Op.like]: `%${title}%`
+				},
+			}
 		}
+
+		const queryOptions: FindOptions<Media> = {
+			where,
+			limit: numLimit,
+			offset: numOffset,
+		}
+
+		const medias = await Media.findAll(queryOptions
+		)
+		res.status(200).json(medias);
 	},
+
 	getMediaById: async (req: Request, res: Response): Promise<void> => {
 		const mediaId = req.params.id;
 
---
+++ src/validators/mediaValidator.ts
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+const getMediasQuerySchema = Joi.object({
+    title: Joi.string().optional(),
+    limit: Joi.number().positive().min(1).max(100).optional(),
+    offset: Joi.number().integer().min(0).optional(),
+})
+
+export { getMediasQuerySchema};
