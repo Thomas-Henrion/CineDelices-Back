4562fc26cb66716b65d41a7cd34a8d263dd2732d
Thomas-Gambin <t.gambin.pro@gmail.com>
2025-04-17 11:16:31 +0200

    feat: add limiter on all route

---
+++ src/middlewares/rateLimitValidator.ts
@@ -0,0 +1,12 @@
+import { rateLimit } from "express-rate-limit"
+
+export const limiter = rateLimit({
+    windowMs: 10 * 1000, // define number for 10 sec
+    limit: 10, // Limit each IP to 10 requests
+    standardHeaders: "draft-8", // Use the last headers format
+    legacyHeaders: false, // disable the older headers format
+    message: {
+        status: 429,
+        error: "Too many requests, please try again later",
+    },
+})
