3433969e33dde07ee350bb8f22383396a55c4c8c
Thomas-Henrion <thomas.henrion@oclock.school>
2025-04-24 12:19:06 +0200

    css changes, view recipes

--- src/controllers/admin/recipesController.ts
+++ src/controllers/admin/recipesController.ts
@@ -1,3 +1,4 @@
+import { valid } from "joi";
 import { Recipe } from "../../database/association";
 import type { Request, Response } from "express";
 
@@ -6,10 +7,24 @@ export default {
     getAllRecipes: async (req: Request, res: Response): Promise<void> => {
         try {
             const recipes = await Recipe.findAll();
-            res.render("/recipes/recipes", {recipes});
+            res.render("recipes/recipes", {recipes});
         } catch (error) {
             res.render("404")
         }
     },
+
+    validateRecipeById: async (req: Request, res: Response): Promise<void> => {
+        const { id } = req.params;
+        try {
+            const recipe = await Recipe.findByPk(id);
+            if (!recipe) {
+                res.status(404).send("Recipe not found");
+                return;
+            }
+            res.render("recipes/validateRecipe", { recipe });
+        } catch (error) {
+            res.status(500).send("Internal server error");
+        }
+    }
     
 }
--- src/routes/admin/recipesRoutes.ts
+++ src/routes/admin/recipesRoutes.ts
@@ -4,6 +4,7 @@ import recipesController from '../../controllers/admin/recipesController';
 const recipesRouter = express.Router();
 
 recipesRouter.get('/', recipesController.getAllRecipes);
+recipesRouter.get('/validate/:id', recipesController.validateRecipeById);
 
 export default recipesRouter
 
